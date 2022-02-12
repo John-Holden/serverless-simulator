c370b2db8404ab1971798c35e9ac8355cb9eb5a9
john.holden11 <john.holden11@nhs.net>
2022-02-12 23:11:40 +0000

    cleaned module dependencies for leaner lambda

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, jsonify, make_response
+import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_from_root():
-    return jsonify(message='Hello from root!')
+    return jsonify(message='Hello from root - homie!')
 
 
 @app.route("/hello")
 def hello():
-    return jsonify(message='Hello from path!')
+    return jsonify(message='Hello from path - homie!')
+
+
+@app.route("/numpy_test")
+def numpy_test():
+    return jsonify(message=f'Hello from numpy_test. array {np.array([1])}!')
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+    return make_response(jsonify(error='Not found - homie!'), 404)
