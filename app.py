4a80a09e62a4deebbef08b6af74c06ab77c263cb
john.holden11 <john.holden11@nhs.net>
2022-02-13 17:23:07 +0000

    1) added dump file to s3 permissions 2) issue with packaging up boto3 for lambda function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response
 import numpy as np
-
+import boto3
+import os
 app = Flask(__name__)
 
 
@@ -17,6 +18,21 @@ def hello():
 @app.route("/numpy_test")
 def numpy_test():
     return jsonify(message=f'Hello from numpy_test. array {np.array([1])}!')
+
+
+@app.route("/s3_upload")
+def upload_to_s3():
+    try:
+        temp_file = open(file='/tmp/temp.upload', mode='w')
+        temp_file.write('test string from local dev')
+        temp_file.close()
+        s3_client = boto3.client('s3')
+        s3_client.upload_file('/tmp/temp.upload', 'tree-epi-site-bucket', 'temp.upload')
+        os.remove('/tmp/temp.upload')
+        return jsonify(message=f'Uploaded to s3!')
+    except Exception as e:
+        print(e)
+        return make_response(jsonify(error=f'{e}'), 404)
 
 
 @app.errorhandler(404)
--- tree_epi_back/src/scripts/run_generic_SIR.py
+++ tree_epi_back/src/scripts/run_generic_SIR.py
@@ -6,10 +6,10 @@ from epidemic_models import executor
 
 
 def run_SIR():
-    runtime = set_runtime(steps=500)
+    runtime = set_runtime(steps=1000)
     dispersal = set_dispersal(model='ga', dispersal_param=50)
-    infection_dynamics = set_infection_dynamics(compartments='SIR', beta_factor=25, pr_approx=False)
-    infectious_lt = set_infectious_lt(distribution='step', life_time_parameter=10)
+    infection_dynamics = set_infection_dynamics(compartments='SIR', beta_factor=10, pr_approx=False)
+    infectious_lt = set_infectious_lt(distribution='step', life_time_parameter=100)
     initial_conditions = set_initial_conditions(distribution='centralised', number_infected=10)
     domain_config = set_domain_config('simple_square', scale_constant=1, tree_density=0.01, patch_size=(500, 500))
     R0_trace = set_R0_trace(active=False, transition_times=True, get_network=True)
